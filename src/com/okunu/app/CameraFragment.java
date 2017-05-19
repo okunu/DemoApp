89bfb51f6c12a4a43c863c07eeca7672e2c8b481
oukun.ok <oukun.ok@alibaba-inc.com>
2017-05-19 15:00:37 +0800

    使用fragment,便于管理页面

---
+++ src/com/okunu/app/CameraFragment.java
@@ -0,0 +1,15 @@
+package com.okunu.app;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CameraFragment extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.camera_layout, container, false);
+    }
+}
--- src/com/okunu/app/FirstActivity.java
+++ src/com/okunu/app/FirstActivity.java
@@ -1,6 +1,9 @@
 package com.okunu.app;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,5 +16,15 @@ public class FirstActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_first);
+        
+        setDefaultFragment(new CameraFragment());
     }
+    
+    public void setDefaultFragment(Fragment fragment){
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.main_content, fragment);
+        transaction.commit();
+    }
+
 }
